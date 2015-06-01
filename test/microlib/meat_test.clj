4415f739d61b07e668fd0e50ce3be115ba8b8a23
Jessica Kerr <jessitron@outpace.com>
2015-06-01 00:07:33 +0000

    failing test to implement test copy-over

--- test/microlib/meat_test.clj
+++ test/microlib/meat_test.clj
@@ -20,10 +20,15 @@
                                           :destproj-file-seq [(file "/Users/fake/destproj-dir/src/dest_proj/libbit/other_libbit.clj")
                                                               (file "/Users/fake/destproj-dir/src/dest_proj/libbit/")] ;; it would contain more but this is the important bit
                                           :libbit-files      [{:location (file "/Users/fake/libbitname-dir/src/libbit/libbit_name.clj")
-                                                               :contents (delay "(ns libbit-name) \"blahblah\" ")}]})]
+                                                               :contents (delay "(ns libbit.libbit-name) \"blahblah\" ")}
+                                                              {:location (file "/Users/fake/libbitname-dir/test/libbit/libbit_name_test.clj")
+                                                               :contents (delay "(ns libbit.libbit-name-test (require [libbit.libbit-name :as subject)) \"blahblah\" ")}]})]
       (is (= {:write {:to       (file "/Users/fake/destproj-dir/src/dest_proj/libbit/libbit_name.clj")
                       :contents "(ns dest-proj.libbit.libbit-name) \"blahblah\" "}}
-             (first result)))))
+             (first result)))
+      (is (= {:write {:to       (file "/Users/fake/destproj-dir/test/dest_proj/libbit/libbit_name_test.clj")
+                      :contents "(ns dest-proj.libbit.libbit-name-test (require [dest-proj.libbit.libbit-name :as subject])) \"blahblah\" "}}
+             (second result)))))
   (testing "Destination has no libbits yet; dir is created"
     ;; this could also be expressed as a property: every write and mkdir command
     ;; returned is either for a directory that exists, or one that has
@@ -35,7 +40,7 @@
                                           :destproj-file-seq [(file "Users/fake/destproj-dir/src/destproj/core.clj") ;; this one has all the dirs we need except libbit
                                                               (file "Users/fake/destproj-dir/src/destproj/")]
                                           :libbit-files      [{:location (file "/Users/fake/libbitname-dir/src/libbit/libbit_name.clj")
-                                                               :contents (delay "(ns libbit-name) \"blahblah\" ")}]})]
+                                                               :contents (delay "(ns libbit.libbit-name) \"blahblah\" ")}]})]
       (is (= {:mkdir {:for (file "/Users/fake/destproj-dir/src/destproj/libbit/libbit_name.clj")}}
              (first result)))
       (is (= {:write {:to       (file "/Users/fake/destproj-dir/src/destproj/libbit/libbit_name.clj")
